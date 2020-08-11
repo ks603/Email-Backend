ea0b70a4d34a56a8645cb3c3c26ca1a2f1ad25e8
ks603 <kevin.shaughnessy18@gmail.com>
2020-08-10 19:22:35 -0500

    Created keys for google authorization

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'you' })
-})
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (acessToken) => {
+      console.log(acessToken)
+    }
+  )
+)
 
 const PORT = process.env.PORT || 5000
 // localhost:5000
