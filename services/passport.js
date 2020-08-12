eb3d0ecffbf083e83d4cfa2723ee447fdf8d222b
ks603 <kevin.shaughnessy18@gmail.com>
2020-08-12 17:45:58 -0500

    You now can only create each user only once

--- services/passport.js
+++ services/passport.js
@@ -13,9 +13,15 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({
-        googleId: profile.id,
-      }).save()
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // we alreeady have that user
+        } else {
+          new User({
+            googleId: profile.id,
+          }).save()
+        }
+      })
     }
   )
 )
