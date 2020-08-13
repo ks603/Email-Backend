7a51006ff84c6458c1e04d0458e3bd25f2f13999
ks603 <kevin.shaughnessy18@gmail.com>
2020-08-13 10:51:39 -0500

    Use cookie-session to keep track of cookies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
+const passport = require('passport')
 const keys = require('./config/keys')
 require('./models/Users')
 require('./services/passport')
@@ -10,6 +12,13 @@ mongoose.connect(keys.mongoURI, {
 })
 
 const app = express()
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+)
 
 require('./routes/authRoutes')(app)
 
