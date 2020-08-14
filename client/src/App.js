da584c64e4653eda6b65db2849a76bdabe04fbbb
ks603 <kevin.shaughnessy18@gmail.com>
2020-08-14 16:47:16 -0500

    Setup proxy for dev server

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h2>Hi there</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href="/auth/google">Sign in with google</a>
     </div>
   )
 }
