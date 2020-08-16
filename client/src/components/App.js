a3794827def48dc175483c179bb18b0984ea023b
ks603 <kevin.shaughnessy18@gmail.com>
2020-08-16 17:20:04 -0500

    Created Header component and installed materialize-css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Header from './Header'
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveryNew = () => <h2>SurveryNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
-  return <div>Hi Threre!</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route path="/surverys" component={Dashboard} />
+        </div>
+      </BrowserRouter>
+    </div>
+  )
 }
 
 export default App
