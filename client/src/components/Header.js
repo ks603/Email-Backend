b6e33912354e94a540808b3da284ede88b4965a2
ks603 <kevin.shaughnessy18@gmail.com>
2020-08-16 18:32:37 -0500

    Added materialize Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,25 @@ import React, { Component } from 'react'
 
 class Header extends Component {
   render() {
-    return <div>Header</div>
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo" style={{ paddingLeft: '30px' }}>
+            Emaily
+          </a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a
+                href="sass.html"
+                style={{ paddingRight: '70px', paddingLeft: '60px' }}
+              >
+                Login With Google
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
   }
 }
 
