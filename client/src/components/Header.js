9fd8f7747fe17a37e348348ecd3ef3044bca89cf
ks603 <kevin.shaughnessy18@gmail.com>
2020-08-17 19:11:57 -0500

    You can now login with Google

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { link } from 'fs'
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        )
+      default:
+        return (
+          <li>
+            <a>Logout</a>
+          </li>
+        )
+    }
+  }
+
   render() {
-    console.log(this.props)
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="brand-logo left">Emaily</a>
           <ul id="nav-mobile" className="right">
-            <li>
-              <a href="sass.html">Login With Google</a>
-            </li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
