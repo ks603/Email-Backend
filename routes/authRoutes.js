59423cead6023c79bfcc23abba9d1e4cbc6339a0
ks603 <kevin.shaughnessy18@gmail.com>
2020-08-17 21:15:22 -0500

    Once verified with google redirect to the dashboard

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,14 @@ module.exports = (app) => {
     })
   )
 
-  app.get('/auth/google/callback', passport.authenticate('google'))
+  app.get(
+    '/auth/google/callback',
+    passport.authenticate('google'),
+    (req, res) => {
+      res.redirect('/surverys')
+    }
+  )
+
   app.get('/api/logout', (req, res) => {
     req.logout()
     res.send(req.user)
