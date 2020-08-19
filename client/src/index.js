b4f7bd9e513c8cf35d359632abb5c48e7deb84c3
ks603 <kevin.shaughnessy18@gmail.com>
2020-08-19 16:25:49 -0500

    Added stripe keys and react-stripe-checkout

--- client/src/index.js
+++ client/src/index.js
@@ -16,3 +16,6 @@ ReactDOM.render(
   </Provider>,
   document.querySelector('#root')
 )
+
+console.log('stripekey is', process.env.REACT_APP_STRIPE_KEY)
+console.log('Environment', process.env.NODE_ENV)
