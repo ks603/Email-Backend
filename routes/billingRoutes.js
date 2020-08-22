956fa6f1351cc50b36ac3f9fc5dd4fd6e55c0dc5
ks603 <kevin.shaughnessy18@gmail.com>
2020-08-21 19:56:55 -0500

    Setup strip payment on the backend

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,3 +1,13 @@
+const keys = require('../config/keys')
+const stripe = require('stripe')
+
 module.exports = (app) => {
-  app.get()
+  app.post('/api/stripe', (req, res) => {
+    stripe.charges.creates({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 for 5 credits',
+      source: req.body.id,
+    })
+  })
 }
