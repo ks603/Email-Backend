3d218616f4bcf22509f749c93759222fc828f3d1
ks603 <kevin.shaughnessy18@gmail.com>
2020-09-10 16:20:48 -0500

    When you send a survey updates user credits

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import reduxThunk from 'redux-thunk'
 
 import App from './components/App'
 import reducers from './reducers'
+
+// development only axios helper
 import axios from 'axios'
 window.axios = axios
 
