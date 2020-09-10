a98a6bd49d6998ae0b57bdee052c94dc8a5eaf2e
ks603 <kevin.shaughnessy18@gmail.com>
2020-09-10 15:27:25 -0500

    Fixed error with the mailer

--- services/Mailer.js
+++ services/Mailer.js
@@ -31,7 +31,7 @@ class Mailer extends helper.Mail {
 
   addRecipients() {
     const personalize = new helper.Personalization()
-    this.recipients.forEach((recipients) => {
+    this.recipients.forEach((recipient) => {
       personalize.addTo(recipient)
     })
     this.addPersonalization(personalize)
