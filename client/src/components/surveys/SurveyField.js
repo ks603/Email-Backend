b63e510a151d0f2cc6eabcbf9fca68bd84894d84
ks603 <kevin.shaughnessy18@gmail.com>
2020-09-12 15:37:59 -0500

    Refactored to make input fields

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+// SurveyField contains logic to render a single lavel and text input
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      <input />
+    </div>
+  )
+}
