a8001b1576ad3a5f7c32b12cc84d50ea5e59eb6a
ks603 <kevin.shaughnessy18@gmail.com>
2020-09-12 15:46:13 -0500

    Added props to surveyfield with reduxform

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,11 +1,10 @@
 // SurveyField contains logic to render a single lavel and text input
 import React from 'react'
 
-export default (props) => {
-  console.log(props.input)
+export default ({ input }) => {
   return (
     <div>
-      <input />
+      <input {...input} />
     </div>
   )
 }
