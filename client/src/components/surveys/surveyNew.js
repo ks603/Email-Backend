284abb9342af1e80d90bda6030c9d63ab64fad16
ks603 <kevin.shaughnessy18@gmail.com>
2020-09-14 18:36:01 -0500

    Created surveyform review component

--- client/src/components/surveys/surveyNew.js
+++ client/src/components/surveys/surveyNew.js
@@ -1,14 +1,19 @@
 // SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from 'react'
 import SurveyForm from './SurveyForm'
+import SurveyFormReview from './SurveyFormReview'
 
 class surveyNew extends Component {
+  state = { showFormReview: false }
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return <SurveyFormReview />
+    }
+    return <SurveyForm />
+  }
   render() {
-    return (
-      <div>
-        <SurveyForm />
-      </div>
-    )
+    return <div>{this.renderContent()}</div>
   }
 }
 
